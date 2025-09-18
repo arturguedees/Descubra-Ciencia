7a2bd59a21f77ef2dcefaf84a269ae0caf85e279
Artur Guedes <rollembergartur@gmail.com>
2025-09-18 16:23:53 -0300

    Add files via upload

---
+++ descubra ciencia/Pages/Home/script.js
@@ -0,0 +1,6 @@
+// exemplo de pesquisa (simulação)
+document.querySelector(".search-box input").addEventListener("keyup", function(e) {
+  if (e.key === "Enter") {
+    alert("Pesquisando por: " + e.target.value);
+  }
+});
